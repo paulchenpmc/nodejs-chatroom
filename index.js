ab414bddf05413d146fe65f8b301ed7f1abe06b8
Paul <paulchenpmc@gmail.com>
2020-03-02 18:11:55 -0700

    Loading js and css files

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + 'index.html');
 });
 
 io.on('connection', function(socket) {
